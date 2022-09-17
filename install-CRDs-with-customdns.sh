dc5311e72622cfe972a2b435dbcb36287104d6b4
scjtqs <scjtqs@qq.com>
2022-09-17 11:52:38 +0800

    fix: certmanager 的dns 改成 119.29.29.29、223.5.5.5 这俩

--- install-CRDs-with-customdns.sh
+++ install-CRDs-with-customdns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install   cert-manager jetstack/cert-manager   --namespace cert-manager   --create-namespace   --version v1.9.1   --set installCRDs=true --set prometheus.enabled=false \
---set 'extraArgs={--acme-http01-solver-nameservers=172.16.119.119:53,--dns01-recursive-nameservers-only,--dns01-recursive-nameservers=172.16.119.119:53,--enable-certificate-owner-ref=true}'
+helm upgrade --install   cert-manager jetstack/cert-manager   --namespace cert-manager   --create-namespace   --version v1.9.1   --set installCRDs=true --set prometheus.enabled=false \
+--set 'extraArgs={--acme-http01-solver-nameservers=119.29.29.29:53\,223.5.5.5:53,--dns01-recursive-nameservers-only,--dns01-recursive-nameservers=119.29.29.29:53\,223.5.5.5:53,--enable-certificate-owner-ref=true}'
