47fa1c7028bb481322523b3b48ef98c262505244
scjtqs <86882590+scjtqs2@users.noreply.github.com>
2022-10-03 17:30:26 +0800

    Update init-k8s-aliyun.sh

--- init-k8s-aliyun.sh
+++ init-k8s-aliyun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kubeadm init --config=kubeadm-config-aliyun.yaml --upload-certs --ignore-preflight-errors=swap
+sudo kubeadm init --config=kubeadm-config-aliyun.yaml --upload-certs --ignore-preflight-errors=swap --ignore-preflight-errors=Mem
