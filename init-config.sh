bcc4b7f93aa2c089f12982a577aa0b113355abc5
scjtqs <86882590+scjtqs2@users.noreply.github.com>
2023-02-08 18:43:50 +0800

    Update init-config.sh

--- init-config.sh
+++ init-config.sh
@@ -35,6 +35,7 @@ sudo sysctl --system
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 ## 安装docker
 echo "开始安装docker"
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
