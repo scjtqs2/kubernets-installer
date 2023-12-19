091615a60699ae6090bf4e497687641158fea2bd
scjtqs <scjtqs@qq.com>
2023-12-19 16:22:35 +0800

    fix: 修正 dnspod-cermanager 和 aliyun源

--- init-config.sh
+++ init-config.sh
@@ -73,7 +73,7 @@ echo "开始安装k8s"
 #curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg
-echo "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/k8s.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/k8s.list
 sudo apt-get update
 sudo apt install -y kubelet=1.23.16-00 kubeadm=1.23.16-00 kubectl=1.23.16-00 --allow-downgrades --allow-change-held-packages
 sudo apt-mark hold kubelet kubeadm kubectl docker-ce docker-ce-cli
