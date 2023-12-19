a2f40bedc67508473daa33b535a4a536eb4dabb7
scjtqs <scjtqs@qq.com>
2023-12-19 18:32:05 +0800

    update: 更新维护到 k8s 1.26.6版本

--- init-config.sh
+++ init-config.sh
@@ -41,32 +41,40 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-#sudo apt-get install docker-ce -y
-VERSION_STRING=5:20.10.24~3-0~ubuntu-jammy
-sudo apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin -y
+# 安装containerd替代docker-ce
+#VERSION_STRING=5:20.10.24~3-0~ubuntu-jammy
+#sudo apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin -y
+#
+#sudo mkdir -p /etc/docker
+#sudo tee /etc/docker/daemon.json<<EOF
+#{
+#    "ipv6": true,
+#    "experimental": true,
+#    "fixed-cidr-v6": "fd00:dead:beef::/48",
+#    "ip6tables": true,
+#  "registry-mirrors": [],
+#    "exec-opts": ["native.cgroupdriver=systemd"],
+#  "log-driver": "json-file",
+#  "log-opts": {
+#    "max-size": "100m"
+#  },
+#  "storage-driver": "overlay2",
+#  "storage-opts": [
+#    "overlay2.override_kernel_check=true"
+#  ]
+#}
+#EOF
+#
+#sudo systemctl enable docker
+#sudo systemctl restart docker
 
-sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json<<EOF
-{
-    "ipv6": true,
-    "experimental": true,
-    "fixed-cidr-v6": "fd00:dead:beef::/48",
-    "ip6tables": true,
-  "registry-mirrors": [],
-    "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOF
-
-sudo systemctl enable docker
-sudo systemctl restart docker
+# 配置 containerd
+sudo apt install -y containerd.io
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo sed -i 's/k8s.gcr.io/registry.aliyuncs.com\/google_containers/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
 
 # 安装k8s
 echo "开始安装k8s"
@@ -75,6 +83,7 @@ echo "开始安装k8s"
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/k8s.list
 sudo apt-get update
-sudo apt install -y kubelet=1.23.16-00 kubeadm=1.23.16-00 kubectl=1.23.16-00 --allow-downgrades --allow-change-held-packages
-sudo apt-mark hold kubelet kubeadm kubectl docker-ce docker-ce-cli
+sudo apt install -y kubelet=1.26.6-00 kubeadm=1.26.6-00 kubectl=1.26.6-00 --allow-downgrades --allow-change-held-packages
+sudo apt-mark hold kubelet kubeadm kubectl
+echo "KUBELET_EXTRA_ARGS=--cgroup-driver=systemd --fail-swap-on=false --max-pods=900"| sudo tee /etc/default/kubelet
 
--- install-CRDs-with-customdns.sh
+++ install-CRDs-with-customdns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm upgrade --install   cert-manager jetstack/cert-manager   --namespace cert-manager   --create-namespace   --version v1.9.1   --set installCRDs=true --set prometheus.enabled=false \
+helm upgrade --install   cert-manager jetstack/cert-manager   --namespace cert-manager   --create-namespace   --version v1.13.3   --set installCRDs=true --set prometheus.enabled=false \
 --set 'extraArgs={--acme-http01-solver-nameservers=119.29.29.29:53\,223.5.5.5:53,--dns01-recursive-nameservers-only,--dns01-recursive-nameservers=119.29.29.29:53\,223.5.5.5:53,--enable-certificate-owner-ref=true}'
